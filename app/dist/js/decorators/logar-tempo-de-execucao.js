59f0794a747fe3747f99c2db897f9d75887ef655
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-07-19 11:51:45 -0300

    Non-functional requirements and log runtime with decorator method

---
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -0,0 +1,14 @@
+export function logarTempoDeExecucao() {
+    return function (target, propertyKey, descriptor) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            const tempoDeExecucao = (t2 - t1) / 1000;
+            console.log(`O método ${propertyKey} levou ${tempoDeExecucao} segundos para ser executado.`);
+            return retorno;
+        };
+        return descriptor;
+    };
+}
