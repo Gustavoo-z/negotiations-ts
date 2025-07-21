8f365e16d0b44ccae6e6967fb0944de03ace1433
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-07-21 13:50:32 -0300

    Property decorator, dynamic getters, Object.defineProperty, and decorator cache

--- app/dist/js/decorators/escapar.js
+++ app/dist/js/decorators/escapar.js
@@ -3,7 +3,6 @@ export function escapar(target, propertyKey, descriptor) {
     descriptor.value = function (...args) {
         let retorno = metodoOriginal.apply(this, args);
         if (typeof retorno === "string") {
-            console.log(`Escape aplicado na classe ${this.constructor.name} para o método ${propertyKey}`);
             retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, "");
         }
         return retorno;
